03c47e25d1c32e91722b098d7844ae6da6c37e2d
mikeoswald <michaelmoswald@gmail.com>
2022-01-06 20:06:47 -0600

    added initial spring project files

---
+++ src/main/java/com/devtoolbox/inspirationalvideoapp/InspirationalVideoAppDynamodbApplication.java
@@ -0,0 +1,13 @@
+package com.devtoolbox.inspirationalvideoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspirationalVideoAppDynamodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InspirationalVideoAppDynamodbApplication.class, args);
+	}
+
+}
